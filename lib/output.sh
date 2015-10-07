0926e5f9ee7101f20d191f2ec925baf1be5f26eb
hideaki.chain@gmail.com <senior.dev.software@gmail.com>
2015-10-07 10:03:46 -0400

    add retry for curl to resolve NPM version

--- lib/output.sh
+++ lib/output.sh
@@ -2,15 +2,16 @@ info() {
   echo "       $*"
 }
 
-# sed has a problem with the huge build output from npm 3
-# try awk? awk  '{ print "       " $0 }'
+# sed -l basically makes sed replace and buffer through stdin to stdout
+# so you get updates while the command runs and dont wait for the end
+# e.g. npm install | indent
 output() {
   local logfile="$1"
   local c='s/^/       /'
 
   case $(uname) in
-    Darwin) tee -a "$logfile" | awk '{ print "       " $0 }';;
-    *)      tee -a "$logfile" | awk -W interactive '{ print "       " $0 }';;
+    Darwin) tee -a "$logfile" | sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
+    *)      tee -a "$logfile" | sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
   esac
 }
 
